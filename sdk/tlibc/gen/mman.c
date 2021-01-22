e59c9ca5e3eac931d9bebfea0fddff276f9bf95a
Your Name <you@example.com>
2021-01-22 18:17:47 +0800

    mman.c

--- sdk/tlibc/gen/mman.c
+++ sdk/tlibc/gen/mman.c
@@ -1,3 +1,10 @@
+#include <stdlib.h>
+#include <sgx_spinlock.h>
+#include "sgx_trts.h"
+#include "sgx_cpuid.h"
+#include "sgx_error.h"
+
+
 #define PAGE_SHIFT      12
 #ifdef __ASSEMBLY__
 #define PAGE_SIZE       (1 << PAGE_SHIFT)
@@ -5,6 +12,10 @@
 #define PAGE_SIZE       (1UL << PAGE_SHIFT)
 #endif
 #define PAGE_MASK       (~(PAGE_SIZE-1))
+
+sgx_status_t sgx_oc_mlock(int * int32_t, uint64_t addr,uint32_t nr_pages);
+sgx_status_t sgx_oc_munlock( int * int32_t ,uint64_t addr,uint32_t nr_pages);
+sgx_status_t sgx_oc_mincore( int * int32_t, uint64_t addr);
 
 int mlock(unsigned long start_address,unsigned long size){
     int res=0;
